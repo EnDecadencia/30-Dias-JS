902d2350fc94518239b5c1042a71e0cb4c060ac2
EnDecadencia <kizihpkier@gmail.com>
2024-02-10 18:47:38 -0300

    Update variables.js

--- Dia1 ejercicios/variables.js
+++ Dia1 ejercicios/variables.js
@@ -7,5 +7,11 @@
 let string = 'esto es una variable de tipo string.'
 let boolean = true
 let number = 18
+let undefinedd
+let null2 = null
 
-console.log(string,boolean,number)
+console.log(string)
+console.log(boolean)
+console.log(number)
+console.log(undefinedd)
+console.log(null2)
