6a5f7a41dcb33ca09edebf41b3858de8b7e25d7c
EnDecadencia <kizihpkier@gmail.com>
2024-02-10 18:37:42 -0300

    archivos dia 1

---
+++ Dia1 ejercicios/variables.js
@@ -0,0 +1,11 @@
+// comentario de JS una linea
+/*
+  comentarios
+        multilinea
+*/
+
+let string = 'esto es una variable de tipo string.'
+let boolean = true
+let number = 18
+
+console.log(string,boolean,number)
