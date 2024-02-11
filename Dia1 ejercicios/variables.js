0e1eabaeec375b66a99548dc60efb3506f2fc16e
EnDecadencia <kizihpkier@gmail.com>
2024-02-11 05:47:56 -0300

    finikitado dia 1

--- Dia1 ejercicios/variables.js
+++ Dia1 ejercicios/variables.js
@@ -9,9 +9,21 @@ let boolean = true
 let number = 18
 let undefinedd
 let null2 = null
+let variable1, variable2, variable3, variable4
+let nombre = 'Alejo'
+let apellido = 'Kiernan Fookes'
+let estadoCivico = 'soltero'
+let pais = 'Argentina'
+let edad = '18'
 
-console.log(string)
-console.log(boolean)
-console.log(number)
-console.log(undefinedd)
-console.log(null2)
+console.log(typeof string)
+console.log(typeof boolean)
+console.log(typeof number)
+console.log(typeof undefinedd)
+console.log(typeof null2)
+console.log(nombre)
+console.log(apellido)
+console.log(estadoCivico)
+console.log(pais)
+console.log(edad)
+console.log("hola mi nombre es: " + nombre + " "+ apellido + " estoy "+ estadoCivico+ " vivo en " + pais + " y tengo " + edad)
