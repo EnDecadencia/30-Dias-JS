299f2169b9c7aa3627cbc7ec3d3f2c59abbb8c2a
EnDecadencia <kizihpkier@gmail.com>
2024-02-13 04:31:51 -0300

    acts lvl1

---
+++ Dia3 ejercicios/main.js
@@ -0,0 +1,40 @@
+const now = new Date()
+
+let firstName = 'Alejo'
+let lastName = 'Fookes Kiernan'
+let country = 'Argentina'
+let city = 'Lanus'
+let age = 18
+let isMarried = false
+const year = now.getFullYear()
+
+console.log(typeof '10' === typeof 10) //2, no es un string
+console.log(parseInt('9.8') === 10) //3, no es 9
+
+//4
+
+console.log(6 > 5)
+console.log(3 == '3')
+console.log(firstName.length < lastName.length)
+
+console.log(6 < 5)
+console.log(3 === '3')
+console.log(firstName.length > lastName.length)
+
+//5 hechoxd
+
+console.log(4 > 3) //true
+console.log(4 >= 3) //true
+console.log(4 < 3) //false
+console.log(4 <= 3) //false
+console.log(4 == 4) //true
+console.log(4 === 4) //true
+console.log(4 != 4) //false
+console.log(4 !== 4) //false
+console.log(4 != '4') //false
+console.log(4 == '4') //true
+console.log(4 === '4') //false
+
+//6
+
+
