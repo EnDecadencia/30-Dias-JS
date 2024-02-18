52f593cfe5ee9012c2b354f6bce298f5daa77a1d
EnDecadencia <kizihpkier@gmail.com>
2024-02-18 06:30:54 -0300

    Update main.js

--- dia4 ejercicios/main.js
+++ dia4 ejercicios/main.js
@@ -35,3 +35,21 @@ if (a > b) {
 //lvl2
 
 //1
+
+var score = prompt("Enter the student's score:");
+
+score = parseInt(score);
+    if (score >= 80 && score <= 100) {
+        console.log("Grade: A");
+    } else if (score >= 70 && score <= 79) {
+        console.log("Grade: B");
+    } else if (score >= 60 && score <= 69) {
+        console.log("Grade: C");
+    } else if (score >= 50 && score <= 59) {
+        console.log("Grade: D");
+    } else if (score >= 0 && score <= 49) {
+        console.log("Grade: F");
+    } else {
+        console.log("Invalid score entered. Please enter a score between 0 and 100.");
+    }
+
