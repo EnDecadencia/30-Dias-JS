2064abefee323cc5e2a779c60fae72e899533e08
EnDecadencia <kizihpkier@gmail.com>
2024-02-18 06:29:07 -0300

    Update main.js

--- dia4 ejercicios/main.js
+++ dia4 ejercicios/main.js
@@ -1,6 +1,37 @@
-let age = prompt('age')
+//lvl 1
+//1
+/*
+let age = prompt('edad')
 let ageNum = parseFloat(age)
 
 if (ageNum  >= 18) {
-    console.log("tenes edad para manejar")
+    alert("tenes edad para manejar")
+} else {
+    alert("no tenes edad para manejar.")
 }
+//2
+let myAge = 18
+let ageUser = prompt('tu edad')
+let ageUserNum = parseInt(ageUser)
+
+if (ageUserNum  > myAge) {
+    difUser = ageUserNum - myAge
+    alert("tenes " + difUser + " años mas años que yo")
+} else {
+    difUser = myAge - ageUserNum
+    alert("Tengo " + difUser + " años mas que vos")
+    }
+    */
+//3
+let a = 30
+let b = 10
+
+if (a > b) {
+    console.log(a + " es mas grande que " + b)
+} else {
+    console.log(b + " es mas grande que " + a)
+}
+
+//lvl2
+
+//1
