b12c01f8e12b3cb0c990e7f9323f40088a98857a
EnDecadencia <kizihpkier@gmail.com>
2024-03-19 03:57:07 -0300

    Update countries.js

--- dia5 ejercicios/countries.js
+++ dia5 ejercicios/countries.js
@@ -1,13 +1,17 @@
-const countries = [
-    'Albania',
-    'Bolivia',
-    'Canada',
-    'Denmark',
-    'Ethiopia',
-    'Finland',
-    'Germany',
-    'Hungary',
-    'Ireland',
-    'Japan',
-    'Kenya'
-  ]
+
+
+  export const myFunction = () => {
+    const countries = [
+        'Albania',
+        'Bolivia',
+        'Canada',
+        'Denmark',
+        'Ethiopia',
+        'Finland',
+        'Germany',
+        'Hungary',
+        'Ireland',
+        'Japan',
+        'Kenya'
+      ]
+};
