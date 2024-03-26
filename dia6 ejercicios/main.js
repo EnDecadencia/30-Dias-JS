2ce7402e8e2950eebad47a10b28e352dccca3589
EnDecadencia <kizihpkier@gmail.com>
2024-03-26 02:48:25 -0300

    acts dia 6

--- dia6 ejercicios/main.js
+++ dia6 ejercicios/main.js
@@ -6,7 +6,28 @@
     //console.log(i)
   //}
 
-  var n = prompt("What number is n? ");
-  for(let i = 0; n >= 0; i++ && n++){ //3
-    console.log(i)
-  }
+/*
+var n = prompt("What number is n? ");
+
+for (var i = 1; i <= n; i++) {
+    console.log(i);
+} */
+//4  var numRows = 6; //4 
+/*
+for (var i = 1; i <= numRows; i++) {
+    var row = '';
+    for (var j = 1; j <= i; j++) {
+        row += '#';
+    }
+    console.log(row);
+} */
+var multiply = 10
+
+for (var i = 1; i <= multiply; i++) {
+  var row = '';
+  for (var j = 1; j <= i; j++) {
+      row = j + ' X ' + j;
+  }   
+  console.log(row);
+}
+
